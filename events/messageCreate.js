1da1f5e28ba4adc41fe5050bf1b0ab3d3e9256b8
BananeChocolat <jy.aurelien@gmail.com>
2024-01-23 21:48:30 +0100

    quoi-feur regexp + modif switch/case link

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -6,7 +6,7 @@ module.exports = async (bot, message) => {
         let text = message.content
     }
 
-	if (message.content.slice(-7).toLowerCase().includes('quoi')) {
+	if (/quoi*\W*$/.test(message.content.toLowerCase())) {
         if (message.author.id === '293463332781031434') {
             message.reply('feu- *pardonnez-moi maître.* :worship_symbol:');
         } else {
@@ -17,39 +17,40 @@ module.exports = async (bot, message) => {
     if (message.content.startsWith('http')) {
         const link = message.content.split(' ')[0].split('/');
         const website = link[2].split('www.').pop();
+        let message_content = "Unknown website";
+        let is_known_website = false;
         switch (website) {
             case "curseforge.com":
-                message.reply({
-                    content: `**${message.author.displayName}**\nhttps://modrinth.com/mod/` + link[5],
-                    allowedMentions: {repliedUser: false}
-                });
+                message_content = `**${message.author.displayName}**\nhttps://modrinth.com/mod/` + link[5]
+                is_known_website = true;
                 break;
             case "twitter.com":
-                message.reply({
-                    content: `**${message.author.displayName}**\n${link.join('/').replace("twitter","fxtwitter")}`,
-                    allowedMentions: {repliedUser: false}
-                });
+                message_content = `**${message.author.displayName}**\n${link.join('/').replace("twitter","fxtwitter")}`
+                is_known_website = true;
                 break;
             case "vm.tiktok.com":
                 const regexp_link = /https.*\d{15,}/;
                 let fetch_res = await fetch(message.content.split(' ')[0], {redirect:'manual'})
                     .then(res => res.text())
                 let full_link = regexp_link.exec(fetch_res)[0]
-                message.reply({
-                    content: full_link.replace("tiktok.com", "vxtiktok.com"),
-                    allowedMentions: {repliedUser: false}
-                })
+                message_content = full_link.replace("tiktok.com", "vxtiktok.com")
+                is_known_website = true;
                 break;
             case "x.com" :
-                message.reply({
-                    content: `**${message.author.displayName}**\n${link.join('/').replace("x","fxtwitter")}`,
-                    allowedMentions: {repliedUser: false}
-                });
+                message_content = `**${message.author.displayName}**\n${link.join('/').replace("x","fxtwitter")}`;
+                is_known_website = true;
                 break;
-            }
+        }
+        if (is_known_website) {
+            message.reply({
+                content: message_content,
+                allowedMentions: {repliedUser: false}
+            });
+        }
+        
     }
 
-    
+
     let prefix = '&';
 
     let messageArray = message.content.split(' ');
